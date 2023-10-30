e2f046b8c0ec1f48e943d3e8dec7dff734534df8
Diego Kleiman <diegokleiman@hotmail.com>
2023-10-30 15:22:09 -0500

    Updated code (must put in script format)

--- esm/esmdynamic/training/training_loop.py
+++ esm/esmdynamic/training/training_loop.py
@@ -3,8 +3,8 @@ Training loop.
 """
 
 import os
-import pickle
 from datetime import datetime
+import pickle
 
 import torch
 from torch.utils.data import DataLoader
@@ -13,6 +13,62 @@ from torch.utils.tensorboard import SummaryWriter
 from esm.esmdynamic.esmdynamic import ESMDynamic
 from esm.esmdynamic.loss import full_form_loss as loss_fn
 from esm.esmdynamic.training import data_reader
+
+
+def cast_bfloat16(batch_labels):
+    """Cast batch of labels to bfloat16.
+    """
+    # Keys to modify
+    change_keys = {"dynamic_contacts", "rmsd"}
+    # Cast auxiliary function
+    cast_bfloat16 = lambda x: x.bfloat16()
+
+    batch_labels = {
+        k: cast_bfloat16(v) if k in change_keys else v for k, v in batch_labels.items()
+    }
+
+    return batch_labels
+
+
+def train_one_epoch(epoch_index, tb_writer):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+        inputs = data_reader.fix_dim_order(inputs)
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        with torch.autocast(device_type="cpu", dtype=torch.bfloat16):
+            outputs = model(precomputed=inputs)
+            labels = cast_bfloat16(labels)
+            # Compute the loss and its gradients
+            loss = loss_fn(outputs, labels) / batch_accum
+
+        loss.backward()
+
+        # Adjust learning weights
+        if ((i + 1) % batch_accum == 0) or (i + 1 == len(training_loader)):
+            optimizer.step()
+            scheduler.step()
+
+            # Gather data and report
+            running_loss += loss.item()
+            if (i + 1) % (batch_accum * 1) == 0:
+                last_loss = running_loss  # loss per batch
+                print('  batch {} loss: {}'.format(i + 1, last_loss))
+                tb_x = epoch_index * len(training_loader) + i + 1
+                tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+                running_loss = 0.
+
+    return last_loss
 
 # Initialize data sets
 with open("../build_dataset/cluster_indices.pkl", "rb") as infile:
@@ -35,8 +91,8 @@ training_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
 validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
 
 # Initialize model
-model = ESMDynamic()
-model.set_chunk_size(128)
+model = ESMDynamic(load_esmfold=False)
+model.set_chunk_size(256)
 
 # Initialize optimizer and scheduler
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, eps=1e-6)
@@ -48,42 +104,6 @@ writer = SummaryWriter('runs/trainer_{}'.format(timestamp))
 epoch_number = 0
 EPOCHS = 5
 best_vloss = 1_000_000
-
-
-# Training code
-def train_one_epoch(epoch_index, tb_writer):
-    running_loss = 0.
-    last_loss = 0.
-
-    for i, data in enumerate(training_loader):
-        # Every data instance is an input + label pair
-        inputs, labels = data
-        inputs = data_reader.fix_dim_order(inputs)
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
-
-        # Make predictions for this batch
-        outputs = model(precomputed=inputs)
-
-        # Compute the loss and its gradients
-        loss = loss_fn(outputs, labels) / batch_accum
-        loss.backward()
-
-        # Adjust learning weights
-        if ((i + 1) % batch_accum == 0) or (i + 1 == len(training_loader)):
-            optimizer.step()
-            scheduler.step()
-
-        # Gather data and report
-        running_loss += loss.item()
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000  # loss per batch
-            print('  batch {} loss: {}'.format(i + 1, last_loss))
-            tb_x = epoch_index * len(training_loader) + i + 1
-            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
-            running_loss = 0.
-
-    return last_loss
 
 
 # Training loop
@@ -103,6 +123,7 @@ for epoch in range(EPOCHS):
     with torch.no_grad():
         for i, vdata in enumerate(validation_loader):
             vinputs, vlabels = vdata
+
             voutputs = model(precomputed=vinputs)
             vloss = loss_fn(voutputs, vlabels)
             running_vloss += vloss
