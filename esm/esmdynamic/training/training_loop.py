b6153afc08a2506905ef5d65d5937aea5f4f903c
Diego Kleiman <diegokleiman@hotmail.com>
2023-10-31 12:05:00 -0500

    Script-format file (untested)

--- esm/esmdynamic/training/training_loop.py
+++ esm/esmdynamic/training/training_loop.py
@@ -3,6 +3,7 @@ Training loop.
 """
 
 import os
+import argparse
 from datetime import datetime
 import pickle
 
@@ -29,6 +30,48 @@ def cast_bfloat16(batch_labels):
 
     return batch_labels
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cluster_indices",
+                        type=str,
+                        help="Path to cluster indices file.",
+                        required=True)
+    parser.add_argument("data_dir",
+                        type=str,
+                        help="Path to input data.",
+                        required=True)
+    parser.add_argument("labels_dir",
+                        type=str,
+                        help="Path to labels.",
+                        required=True)
+    parser.add_argument("outpath",
+                        type=str,
+                        help="Path where output will be stored.",
+                        required=True)
+    parser.add_argument("batch_size",
+                        type=int,
+                        help="Batch size.",
+                        default=1)
+    parser.add_argument("batch_accum",
+                        type=int,
+                        help="Number of batches to accumulate before update.")
+    parser.add_argument("epochs",
+                        type=int,
+                        help="Number of epochs.")
+    parser.add_argument("split_lengths",
+                        type=int,
+                        nargs="+",
+                        help="Train, val, test samples.")
+    parser.add_argument("pretrained",
+                        type=str,
+                        help="Path to pretrained model.")
+    parser.add_argument("device",
+                        type=str,
+                        help="Training will run here.",
+                        default="cpu")
+
+    return parser.parse_args()
+
 
 # Initialize data sets
 def init_datasets(cluster_index_file, data_dir, labels_dir, split_lengths=None):
@@ -52,14 +95,16 @@ def init_datasets(cluster_index_file, data_dir, labels_dir, split_lengths=None):
     return training_set, validation_set, testing_set
 
 
-def init_data_loaders(training_set, validation_set, batch_size=1, batch_accum=16):
+def init_data_loaders(training_set, validation_set, batch_size=1):
     training_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
     return training_loader, validation_loader
 
 
-def init_model(chunk_size=256, device="cpu"):
+def init_model(chunk_size=256, device="cpu", pretrained=None):
     model = ESMDynamic(load_esmfold=False)
+    if pretrained:
+        model.load_state_dict(torch.load(pretrained))
     model.set_chunk_size(chunk_size)
     if device == "cuda":
         model.cuda()
@@ -67,16 +112,18 @@ def init_model(chunk_size=256, device="cpu"):
     return model
 
 
-def init_optimizer(model, lr=0.001, eps=1e-6, sch_iters=10):
+def init_optimizer(model, num_epochs, lr=0.001, eps=1e-6):
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, eps=eps)
-    scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, total_iters=sch_iters)
+    total_iters = int(num_epochs*0.1)
+    scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=0.1, total_iters=total_iters)
     return optimizer, scheduler
 
 
-def init_writer():
+def init_writer(outpath):
     # Initialize other parameters for the run
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    return timestamp, SummaryWriter('runs/trainer_{}'.format(timestamp))
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    writer = SummaryWriter(os.path.join(outpath, "trainer_{}".format(timestamp)))
+    return timestamp, writer
 
 
 def train_one_epoch(training_loader,
@@ -131,6 +178,7 @@ def run_training(model,
                  scheduler,
                  num_epochs,
                  writer,
+                 outpath,
                  device,
                  batch_accum,
                  timestamp):
@@ -162,7 +210,7 @@ def run_training(model,
         with torch.no_grad():
             for i, vdata in enumerate(validation_loader):
                 vinputs, vlabels = vdata
-
+                vinputs = data_reader.fix_dim_order(vinputs)
                 voutputs = model(precomputed=vinputs)
                 vloss = loss_fn(voutputs, vlabels)
                 running_vloss += vloss
@@ -180,11 +228,37 @@ def run_training(model,
         # Track best performance, and save the model's state
         if avg_vloss < best_vloss:
             best_vloss = avg_vloss
-            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+            model_path = os.path.join(outpath, "model_{}_{}".format(timestamp, epoch_number))
             torch.save(model.state_dict(), model_path)
 
         epoch_number += 1
 
 
 if __name__ == "__main__":
-    pass
+
+    args = get_args()
+    assert(len(args.split_lengths) == 3)
+    training_set, validation_set, testing_set = init_datasets(args.cluster_indices,
+                                                              args.data_dir,
+                                                              args.labels_dir,
+                                                              split_lengths=args.split_lengths)
+    training_loader, validation_loader = init_data_loaders(training_set,
+                                                           validation_set,
+                                                           batch_size=args.batch_size)
+    model = init_model(device=args.device, pretrained=args.pretrained)
+    optimizer, scheduler = init_optimizer(model, args.epochs)
+    timestamp, writer = init_writer(args.outpath)
+
+    run_training(model,
+                 training_loader,
+                 validation_loader,
+                 optimizer,
+                 scheduler,
+                 args.epochs,
+                 writer,
+                 args.outpath,
+                 args.device,
+                 args.batch_accum,
+                 timestamp)
+
+
