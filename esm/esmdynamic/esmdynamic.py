32ceba16071f505f9278454c35054c0db1e7ad69
Diego Kleiman <diegokleiman@hotmail.com>
2023-11-21 13:49:00 -0600

    Some debugging statements...

--- esm/esmdynamic/esmdynamic.py
+++ esm/esmdynamic/esmdynamic.py
@@ -158,7 +158,7 @@ class ESMDynamic(nn.Module):
                                                     structure['mask'],
                                                     no_recycles=num_recycles
                                                     )
-        print(dynamic_module_output)
+        print(torch.any(torch.isnan(dynamic_module_output['s_z'])))
         # dynamic_module_output += dynamic_module_output.T  # Symmetrize the output
         structure['dynamic_module_output'] = dynamic_module_output
 
